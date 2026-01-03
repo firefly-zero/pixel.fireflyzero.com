40c1e4e9a6717bfbc12ac5ba11c7e8746d004122
gram <git@orsinium.dev>
2026-01-03 13:37:21 +0100

    more palettes

--- build.js
+++ build.js
@@ -1,77 +1,78 @@
-const fs = require('fs');
-const path = require('path');
-const gulp = require('gulp');
-const include = require('gulp-include');
-const handlebars = require('gulp-hb');
-const sass = require('gulp-sass')(require('sass'));
-const rename = require('gulp-rename');
+const fs = require("fs");
+const path = require("path");
+const gulp = require("gulp");
+const include = require("gulp-include");
+const handlebars = require("gulp-hb");
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
 
 //const hb_svg = require('handlebars-helper-svg');
 
-const BUILDDIR = process.argv[2] || './build/';
+const BUILDDIR = process.argv[2] || "./build/";
 
+console.log("Building Pixel Editor");
 
-console.log('Building Pixel Editor');
-
-
-function copy_images(){
-    // Icons
-    gulp.src('./images/*.png').pipe(gulp.dest(BUILDDIR));
-	//favicon
-    gulp.src('./images/*.ico').pipe(gulp.dest(BUILDDIR));
-    // Splash images
-    gulp.src('./images/Splash images/*.png').pipe(gulp.dest(BUILDDIR));
-    // Logs gifs
-    gulp.src('./images/Logs/*.gif').pipe(gulp.dest(BUILDDIR));
-    // Logs pngs
-    gulp.src('./images/Logs/*.png').pipe(gulp.dest(BUILDDIR));
-    // Tool tutorials
-    gulp.src('./images/ToolTutorials/*.gif').pipe(gulp.dest(BUILDDIR));
+function copy_images() {
+  // Icons
+  gulp.src("./images/*.png").pipe(gulp.dest(BUILDDIR));
+  //favicon
+  gulp.src("./images/*.ico").pipe(gulp.dest(BUILDDIR));
+  // Splash images
+  gulp.src("./images/Splash images/*.png").pipe(gulp.dest(BUILDDIR));
+  // Logs gifs
+  gulp.src("./images/Logs/*.gif").pipe(gulp.dest(BUILDDIR));
+  // Logs pngs
+  gulp.src("./images/Logs/*.png").pipe(gulp.dest(BUILDDIR));
+  // Tool tutorials
+  gulp.src("./images/ToolTutorials/*.gif").pipe(gulp.dest(BUILDDIR));
 }
 
 function copy_logs() {
-    gulp.src('logs/*.html').pipe(gulp.dest(BUILDDIR));
+  gulp.src("logs/*.html").pipe(gulp.dest(BUILDDIR));
 }
 
-function render_js(){
-    gulp.src('./js/pixel-editor.js')
-        .pipe(include({includePaths: [
-            'js',
-            '!js/_*.js',
-        ]}))
-        .on('error', console.log)
-        .pipe(gulp.dest(BUILDDIR));
+function render_js() {
+  gulp
+    .src("./js/pixel-editor.js")
+    .pipe(include({ includePaths: ["js", "!js/_*.js"] }))
+    .on("error", console.log)
+    .pipe(gulp.dest(BUILDDIR));
 }
 
-
-function render_css(){
-    gulp.src('css/*.scss')
-        .pipe(sass({includePaths: ['css']}))
-        .pipe(gulp.dest(BUILDDIR));
+function render_css() {
+  gulp
+    .src("css/*.scss")
+    .pipe(sass({ includePaths: ["css"] }))
+    .pipe(gulp.dest(BUILDDIR));
 }
 
-function compile_page(){
-    gulp.src(path.join('./views/index.hbs'))
-		.pipe(include({includePaths: ['/svg']}))
+function compile_page() {
+  gulp
+    .src(path.join("./views/index.hbs"))
+    .pipe(include({ includePaths: ["/svg"] }))
 
-        .pipe(handlebars({encoding: 'utf8', debug: true, bustCache: true})
-            .partials('./views/[!index]*.hbs').partials('./views/popups/*.hbs')
-            .partials('./views/components/*.hbs')
-            .helpers('./helpers/**/*.js')
-            .data({
-                projectSlug: 'pixel-editor',
-                title: 'Lospec Pixel Editor',
-                layout: false,
-            }))
-        .pipe(rename('index.htm'))
-        .pipe(gulp.dest(BUILDDIR));
+    .pipe(
+      handlebars({ encoding: "utf8", debug: true, bustCache: true })
+        .partials("./views/[!index]*.hbs")
+        .partials("./views/popups/*.hbs")
+        .partials("./views/components/*.hbs")
+        .helpers("./helpers/**/*.js")
+        .data({
+          projectSlug: "pixel-editor",
+          title: "Pixel Editor",
+          layout: false,
+        })
+    )
+    .pipe(rename("index.htm"))
+    .pipe(gulp.dest(BUILDDIR));
 }
-
 
 // empty the build folder, or create it
 try {
-fs.rmdirSync(BUILDDIR, { recursive: true });
-fs.mkdirSync(BUILDDIR);
-} catch (err) {console.log('failed to find build folder, but it\'s probably fine')}
+  fs.rmdirSync(BUILDDIR, { recursive: true });
+  fs.mkdirSync(BUILDDIR);
+} catch (err) {
+  console.log("failed to find build folder, but it's probably fine");
+}
 
 gulp.parallel(copy_images, copy_logs, render_js, render_css, compile_page)();
